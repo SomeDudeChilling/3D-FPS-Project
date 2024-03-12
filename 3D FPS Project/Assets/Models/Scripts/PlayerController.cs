e2e0bbf4e709e786730d91eb5b4613880b1779d7
SomeDudeChilling <jonathonacosta591@riversideunified.org>
2024-03-12 14:23:37 -0700

    working on jumping

--- 3D FPS Project/Assets/Models/Scripts/PlayerController.cs
+++ 3D FPS Project/Assets/Models/Scripts/PlayerController.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1f;
+    public float jumpForce = 10f;
     public float mouseSensitivity = 1f;
+    public float gravityModifier = 1f;
     public Transform theCamera;
+    public Transform groundCheckpoint;
+    public LayerMask whatIsGround;
+    private bool _canPlayerJump;
     private Vector3 _moveInput;
-
     private CharacterController _characterController;
-
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +25,38 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Player movement
-        _moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        _moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        //Player jump setup
+        float yVelocity = _moveInput.y;
 
-        _characterController.Move(_moveInput);
+        _moveInput.y = yVelocity;
+
+        _moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+        
+        //Check if character controller is on the ground
+        if(_characterController.isGrounded)
+        {
+            _moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+        }
+
+        //Check if player can jump
+        _canPlayerJump = Physics.OverlapSphere(groundCheckpoint.position, 0.5f, whatIsGround).Length > 0; 
+
+        if(Input.GetKeyDown(KeyCode.Space) && _canPlayerJump)
+        {
+            _moveInput.y = jumpForce;
+        }
+
+        //Player movement
+        //_moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //_moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        Vector3 forwardDirection = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horizontalDirection = transform.right * Input.GetAxis("Horizontal");
+
+        _moveInput = (forwardDirection + horizontalDirection).normalized;
+        _moveInput *= moveSpeed;
+
+        _characterController.Move(_moveInput * Time.deltaTime);
 
         //Camera rotation
         Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
